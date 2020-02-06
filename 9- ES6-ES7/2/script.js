23b318a2dcf5bb90206bd53ee62e789dc0bb2066
Hakan-unal <hakanunal_1996@hotmail.com>
2020-02-07 01:01:10 +0300

    updated

---
+++ 9- ES6-ES7/2/script.js
@@ -0,0 +1,85 @@
+// *** Map
+
+
+/*
+let val;
+
+let numbers = new Map();
+
+numbers.set(1, "one");
+numbers.set(2, "two");
+numbers.set(3, "three");
+numbers.set("four", "four4");
+
+val = numbers;
+val = numbers.get(1);
+val = numbers.get(2);
+val = numbers.get(3);
+val = numbers.get("four");
+val = numbers.size;
+val = numbers.has(1);
+val = numbers.has("four");
+val = numbers.has("fou");
+
+for (let [key, value] of numbers) {
+    console.log(key + "=" + value);
+}
+
+for (let [key, value] of numbers.entries()) {
+    console.log(key + "=" + value);
+}
+
+for (let key of numbers.keys()) {
+    console.log("key" + " " + key);
+}
+
+for (let value of numbers.values()) {
+    console.log("value" + " " + value);
+}
+console.log(val);
+
+
+
+let first = new Map([
+    [1, "one"],
+    [2, "two"],
+    [3, "three"]
+]);
+
+let second = new Map([
+    [4, "four"],
+    [5, "five"]
+]);
+
+let merged = new Map([...first, ...second]);
+
+console.log(merged);
+*/
+
+
+
+// *** Sets
+
+
+/*
+let val;
+let mySet = new Set();
+
+mySet.add(1);
+mySet.add(1);
+mySet.add(2);
+mySet.add(2);
+mySet.add("merhaba");
+let obj = { a: 1, b: 2 };
+mySet.add(obj);
+
+val = mySet.has(1);
+val = mySet.has(3);
+
+console.log(mySet);
+console.log(val);
+
+for (let item of mySet) {
+    console.log(item);
+}
+*/
