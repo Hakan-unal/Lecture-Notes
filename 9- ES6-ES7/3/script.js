c5a527a073f35ac9753aa8f5e2145c4b48503ed5
Hakan-unal <hakanunal_1996@hotmail.com>
2020-02-07 01:27:57 +0300

    updated

---
+++ 9- ES6-ES7/3/script.js
@@ -0,0 +1,55 @@
+// Class
+
+
+/*
+class Bilgi {
+    constructor(name, surname, number) {
+        this.name = name;
+        this.surname = surname;
+        this.no = number;
+    }
+
+    calculate() {
+        return 5 + this.no;
+    }
+
+    static fonki() {
+        return 5;
+    }
+}
+
+let object = new Bilgi("Hakan", "Ünal", 12);
+
+console.log(object.calculate());
+console.log(Bilgi.fonki());
+// console.log(object.fonk());  // error
+
+
+class Person {
+    constructor(tc) {
+        this.tc = tc;
+    }
+    fonki() {
+        return "merhaba";
+    }
+}
+
+class Bilgi1 extends Bilgi {
+    constructor(name, surname, number, say) {
+        super(name, surname, number);
+        this.say = say;
+    }
+
+    method() {
+        return this.say;
+    }
+    static fonk() {
+        return this.say + " " + this.name;
+    }
+}
+
+let object1;
+object1 = new Bilgi1("hakan", "ünal", "321", "merhaba");
+
+console.log(object1.method());
+*/
