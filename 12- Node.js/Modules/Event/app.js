18a7bc0297d01a71f3527db422b9f4a4fb4d8bc1
Hakan-unal <hakanunal_1996@hotmail.com>
2020-02-13 22:07:00 +0300

    updated

--- 12- Node.js/Modules/Event/app.js
+++ 12- Node.js/Modules/Event/app.js
@@ -1,8 +1,11 @@
+// fileLogger değişkenine ayı dizinde bulunan logger.js dosyası require methodu ile import edildi
 const fileLogger = require("./logger");
-classLogger = fileLogger.Logger;
-let object = new classLogger();
 
+// object objesi fileLogger içinde bulunan Logger() class'ı üzerinden yaratıldı
+let object = new fileLogger.Logger();
 
+// obje üzerinde yer alan on() methodu bir event bekliyor. Tetikleme gerçekleştiğinde tetiklemenin
+// yanında gönderilen parametre fonksiyon içerisine girecek
 object.on("connection", function (args) {
     console.log(`bağlantı kuruldu.`,args);
 });
--- 12- Node.js/Modules/Event/logger.js
+++ 12- Node.js/Modules/Event/logger.js
@@ -1,12 +1,20 @@
 const Event = require("events");
-
+// Event değişkenine events modülü tanımlandı. Aşağıda bulunan Logger class'ı oluşturulurken Event class'ı 
+// içerisindeki özellikler yeni class'a extend edildi
 class Logger extends Event {
-
+    // log fonksiyonu içerisine gelen message parametresi üzeridnen aşağıdaki işlemler gerçekleşiyor
     log(message) {
         console.log(message);
-        let object = { name: "Hakan", surname: "Ünal" }
+        let object = { name: "Hakan", surname: "Ünal" };
+        // Bu class üzerinden oluşturulan obje this parametresi ile emit methodunu içine alır (emit: event 
+        // tetiklemeye yarıyor aşağıda event'i tetikleyecek olay "connection") bu methodun içine object
+        // değişkeninide gönderir tetiklenmenin beklendiği yer değişkenAdı.on("connection") methodu ayrıca
+        // parametre beklendiği için değişkenAdı.on("connection",(args)=>{}) böyle bir fonksiyon tarafından
+        // event bekleniyor
         this.emit("connection", object);
     }
 }
 
+// require edilmesi halinde üzerine tanımlandığı değişken üzerinden değişkenAdı.Logger ile buradaki
+// Logger class'ına ait tüm özelliklerden obje yaratılabilir
 module.exports.Logger = Logger;
