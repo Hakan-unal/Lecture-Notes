8c6885aff79c547847c1f2112b8a8e848bbccfba
Hakan-unal <hakanunal_1996@hotmail.com>
2020-02-16 00:49:50 +0300

    updated

--- 16- MVC/express-app/controllers/products.js
+++ 16- MVC/express-app/controllers/products.js
@@ -1,12 +1,9 @@
-const products = [
-    { name: 'Samsung S8', price: 3000, image: '1.jpg', description: 'iyi telefon' },
-    { name: 'Samsung S7', price: 2000, image: '2.jpg', description: 'idare eder' },
-    { name: 'Samsung S9', price: 4000, image: '3.jpg', description: 'çok iyi' },
-    { name: 'IPhone 7S', price: 4500, image: '4.jpg', description: 'güzel telefon' }
-];
+const Product = require('../models/product');
 
 
 exports.getProducts = (req, res, next) => {
+
+    const products = Product.getAll();
     res.render('index',
         {
             title: 'Homepage',
@@ -24,13 +21,12 @@ exports.getAddProduct = (req, res, next) => {
 }
 
 exports.postAddProduct = (req, res, next) => {
-    products.push({
-        name: req.body.name,
-        price: req.body.price,
-        image: req.body.image,
-        description: req.body.description
-    });
+    const product = new Product(
+        req.body.name,
+        req.body.price,
+        req.body.imageUrl,
+        req.body.description);
 
-    console.log(req.body);
+    product.saveProduct();
     res.redirect('/');
 }
