154544bfd51f4942d81b6e14c86169cd8ae23387
Hakan-unal <hakanunal_1996@hotmail.com>
2020-03-01 02:22:26 +0300

    updated

---
+++ 12- Node.js/ModulExport/scriptA.js
@@ -0,0 +1,34 @@
+// Browser
+
+
+/*
+const ControlA = (() => {
+    let name, surname;
+    name = "HakanA";
+    surname = "ÜnalA";
+    return {
+        name,
+        surname
+    }
+})();
+*/
+
+
+// Node.js
+
+
+/*
+const denemeNode = require("./scriptB");
+
+denemeNode.deneme("Tugay Ünal");
+*/
+
+
+
+export function fonksiyon1(item) {
+    console.log(item);
+}
+
+export function fonksiyon2(item) {
+    console.log(item);
+}
